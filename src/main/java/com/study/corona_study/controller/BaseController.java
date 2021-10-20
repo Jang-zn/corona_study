f19801a9b9075f721d3b061b6dc8c8e1c538f380
Jang <zn2309@naver.com>
2021-10-20 22:06:27 +0900

    index페이지 추가함
    타임리프 쓰면 세팅 안해줘도 된다고 한다.

--- src/main/java/com/study/corona_study/controller/BaseController.java
+++ src/main/java/com/study/corona_study/controller/BaseController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class BaseController {
 
-    //루트 - 메인페이지 띄워줄거 설정
+    //루트 - 메인페이지 띄워줄거 설정함.
+    //페이지 설정 안해놓으면 기본적으로 JSP view파일을 찾으려고 함.
+    //main 내부의 webapp 안에 view 넣어주면 됨.
+    //JSP쓸거면 재스퍼 디펜던시 추가해줘야되고 암튼 더 뭔가 해줘야됨.
     @GetMapping("/")
     public String root(){
         return "index";
     }
+
+
 
 
 }
