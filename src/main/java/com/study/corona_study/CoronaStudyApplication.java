9b87dd5c6a01d6314a12722c0d955246b18dc48d
Jang <zn2309@naver.com>
2021-10-20 21:49:10 +0900

    Initial commit

---
+++ src/main/java/com/study/corona_study/CoronaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.corona_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaStudyApplication.class, args);
+    }
+
+}
