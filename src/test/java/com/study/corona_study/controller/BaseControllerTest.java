39268dd592846ec2fcb14d876a99d66ed9747c68
Jang <zn2309@naver.com>
2021-10-27 15:25:17 +0900

    Controller Test 작성중

---
+++ src/test/java/com/study/corona_study/controller/BaseControllerTest.java
@@ -0,0 +1,28 @@
+package com.study.corona_study.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class BaseControllerTest {
+
+    private MockMvc mvc;
+
+    @Test
+    void root(){
+        //Given
+
+
+        //When
+
+
+        //Then
+    }
+}
