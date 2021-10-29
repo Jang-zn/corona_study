1b46899cefb81b3e39e2b34ecb70b3228cfc4d01
Jang <zn2309@naver.com>
2021-10-29 21:36:23 +0900

    BaseErrorControllerTest 작성
    잘못된 uri 요청시 404 리턴해주는 테스트

---
+++ src/test/java/com/study/corona_study/error/BaseErrorControllerTest.java
@@ -0,0 +1,35 @@
+package com.study.corona_study.error;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class BaseErrorControllerTest {
+    private final MockMvc mvc;
+
+    public BaseErrorControllerTest(@Autowired MockMvc mvc){
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[view][GET]잘못된 uri요청시 404 리턴")
+    @Test
+    void givenWrongURI_whenRequestingPage_then404ErrorPage() throws Exception{
+        //Given
+
+        //When&Then
+        mvc.perform(get("/asdf"))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+}
